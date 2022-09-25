4137338e60350e857bbcadbbe17bd51335c6802c
Sergio Hernández Navarro <sergio.h.navarro@finetwork.com>
2022-09-25 14:31:10 +0200

    create model and context

---
+++ Database/BD/Context/MyContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.BD.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> data) : base(data) { }       
+    }
+}
